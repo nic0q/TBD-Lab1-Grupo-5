530928064093846164341e506414d903ab096061
Nícolas Farfán <nicolas.farfan.ch@usach.cl>
2022-08-25 20:54:24 -0400

    feat: ej8 ej9

--- dvd_rental_querys.sql
+++ dvd_rental_querys.sql
@@ -88,14 +88,34 @@ where rt.customer_id = 481
 order by rt.return_date; -- por defecto es descendente
 -- Ejercicio 8
 -- a
-select f.title, rt.return_date
-from rental as rt
-inner join inventory as inv on inv.inventory_id = rt.inventory_id
-inner join film as f on f.film_id = inv.film_id
+select cat.name, count(cat.category_id) as cantidad
+from category as cat
+inner join film_category as fcat on fcat.category_id = cat.category_id -- obtengo las categorias
+inner join film as f on f.film_id = fcat.film_id -- obtengo las categorias de las peliculas
+inner join inventory as inv on inv.film_id = f.film_id -- obtengo las peliculas rentadas
+inner join rental as rt on rt.inventory_id = inv.inventory_id
 where rt.customer_id = 481
-order by rt.return_date;
+group by cat.category_id;	
 -- b
-select cus.customer_id
-from customer as cus
-where cus.customer_id = 481;
-inner join
+select cat.name, count(cat.category_id) as cantidad
+from category as cat
+inner join film_category as fcat on fcat.category_id = cat.category_id -- obtengo las categorias
+inner join film as f on f.film_id = fcat.film_id -- obtengo las categorias de las peliculas
+inner join inventory as inv on inv.film_id = f.film_id -- obtengo las peliculas rentadas
+inner join rental as rt on rt.inventory_id = inv.inventory_id
+where rt.customer_id = 481
+group by cat.category_id
+order by cantidad
+DESC
+limit 3;
+-- Ejercicio 9
+-- a
+select count(distinct(f.film_id)) as Cantidad
+from film as f
+inner join inventory as inv on inv.film_id = f.film_id
+inner join rental as rt on rt.inventory_id = inv.inventory_id
+inner join customer as cust on cust.customer_id = rt.customer_id
+inner join address as addr on addr.address_id = cust.address_id
+inner join city as cty on cty.city_id = addr.city_id
+inner join country as co on co.country_id = cty.country_id
+where co.country = 'Canada';
