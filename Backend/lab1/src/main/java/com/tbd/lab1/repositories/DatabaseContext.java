1fdb156a7cb5f3264ea505bfb83edd3e74152da8
Nícolas Farfán <nicolas.farfan.ch@usach.cl>
2022-10-05 23:19:38 -0300

    feat: sqlo2

--- Backend/lab1/src/main/java/com/tbd/lab1/repositories/DatabaseContext.java
+++ Backend/lab1/src/main/java/com/tbd/lab1/repositories/DatabaseContext.java
@@ -1,31 +1,24 @@
 package com.tbd.lab1.repositories;
 
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.sql2o.Sql2o;
 
-
-@Configuration
+@Component
 public class DatabaseContext {
-    //Definir url de la BD, usuario y password
-    //Ejemplo: jdbc:postgresql://127.@.0.1:5432/postgres, usuario, password
-    /*
-    @Bean
-    public Sql2o sql20(){
-        return new Sql20("", "", "");
-    }*/
-    @Value("${database.url}")
-    private String dbUrl;
-
-    @Value("${database.user}")
-    private String dbUser;
-
-    @Value("${database.password}")
-    private String dbPass;
-
-    @Bean
-    public Sql2o sql2o(){
-        return new Sql2o(dbUrl, dbUser, dbPass);
+    @Configuration
+    public class DataBaseContext {
+        @Value("${database.url}")
+        private String dbUrl;
+        @Value("${database.user}")
+        private String dbUser;
+        @Value("${database.password}")
+        private String dbPass;
+        @Bean
+        public Sql2o sql2o() {
+            return new Sql2o(dbUrl, dbUser, dbPass);
+        }
     }
 }
