d80584b7b74838033654dbe75042683ed4c7af37
Vanina Correa <vanina.correa@usach.cl>
2022-10-09 11:37:12 -0300

    fix: arregla error pequeño en model de Task

--- Backend/src/main/java/com/tbd/lab1/models/Task.java
+++ Backend/src/main/java/com/tbd/lab1/models/Task.java
@@ -3,8 +3,8 @@ package com.tbd.lab1.models;
 public class Task {
     private Integer id_task;
     private String description;
-    private Integer fk_emergency;
-    private Integer fk_state_task;
+    private Integer id_emergency;
+    private Integer id_state_task;
 
 
     public Integer getId() {
@@ -24,19 +24,19 @@ public class Task {
     }
 
     public Integer getFk_emergency() {
-        return fk_emergency;
+        return id_emergency;
     }
 
     public void setFk_emergency(Integer fk_emergency) {
-        this.fk_emergency = fk_emergency;
+        this.id_emergency = fk_emergency;
     }
 
     public Integer getFk_state_task() {
-        return fk_state_task;
+        return id_state_task;
     }
 
     public void setFk_state_task(Integer fk_state_task) {
-        this.fk_state_task = fk_state_task;
+        this.id_state_task = fk_state_task;
     }
 
     @Override
@@ -44,8 +44,8 @@ public class Task {
         return "Task{" +
                 "id_task=" + id_task +
                 ", description='" + description + '\'' +
-                ", fk_emergency=" + fk_emergency +
-                ", fk_state_task=" + fk_state_task +
+                ", fk_emergency=" + id_emergency +
+                ", fk_state_task=" + id_state_task +
                 '}';
     }
 }
