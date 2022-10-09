12d983b26412633b4b891a35119e8fc5074eb701
Nicolas Venegas <nicolas.venegas.a@usach.cl>
2022-10-09 20:40:28 -0300

    fix post query

--- Backend/src/main/java/com/tbd/lab1/repositories/AbilityRepositoryImp.java
+++ Backend/src/main/java/com/tbd/lab1/repositories/AbilityRepositoryImp.java
@@ -8,11 +8,18 @@ import org.sql2o.Sql2o;
 
 import java.util.List;
 
+/**
+ * Clase que implementa la interfaz AbilityRepository
+ */
 @Repository
 public class AbilityRepositoryImp implements AbilityRepository {
 
     @Autowired
     private Sql2o sql2o;
+    /**
+     * Método que retorna una lista de todas las tablas de Ability
+     * @return List<Ability>
+     */
     @Override
     public List<Ability> getAllAbilities() {
         try(Connection conn = sql2o.open()){
@@ -23,6 +30,10 @@ public class AbilityRepositoryImp implements AbilityRepository {
             return null;
         }
     }
+    /**
+     * Metodo que retorna un objeto de tipo Ability, dado un id
+     * @return List<Ability>
+     */
     @Override
     public List<Ability> getAbilityById(Integer id) {
         try(Connection conn = sql2o.open()){
@@ -34,11 +45,17 @@ public class AbilityRepositoryImp implements AbilityRepository {
             return null;
         }
     }
+    /**
+     * Método que recibe un objeto de tipo Ability y lo inserta en la base de datos.
+     * si no se pudo insertar retorna null.
+     * @param ability
+     * @return Ability
+     */
     @Override
     public Ability createAbility(Ability ability) {
         try(Connection conn = sql2o.open()){
             conn.createQuery("INSERT INTO \"Ability\" (name_ability, description_ability) " +
-            "VALUES (:name_ability,:description_ability")
+            "VALUES (:name_ability,:description_ability)")
                     .addParameter("name_ability", ability.getName_ability())
                     .addParameter("description_ability", ability.getDescription_ability())
                     .executeUpdate().getKey();
@@ -48,6 +65,12 @@ public class AbilityRepositoryImp implements AbilityRepository {
             return null;
         }
     }
+    /**
+     * Método que recibe un objeto de tipo Ability y lo actualiza en la base de datos mediante su id.
+     * si no se pudo actualizar retorna false.
+     * @param ability
+     * @return boolean
+     */
     @Override
     public boolean editAbility(Ability ability) {
         try(Connection conn = sql2o.open()){
@@ -62,6 +85,12 @@ public class AbilityRepositoryImp implements AbilityRepository {
             return false;
         }
     }
+    /**
+     * Método que recibe un id de tipo Integer y elimina un objeto de tipo Ability de la base de datos.
+     * si no se pudo eliminar retorna false.
+     * @param id
+     * @return boolean
+     */
     @Override
     public boolean deleteAbility(Integer id) {
         int deletedAbility;
@@ -73,6 +102,10 @@ public class AbilityRepositoryImp implements AbilityRepository {
         return deletedAbility == 1;
     }
 
+    /**
+     * Método elimina todas las abilities de la base de datos.
+     * @return List<Ability>
+     */
     @Override
     public int deleteAllAbility(){
         try(Connection conn = sql2o.open()){
